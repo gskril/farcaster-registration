3c832c5a975afa22625613bee4c590ab99906b6a
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-10-14 17:16:44 -0400

    Create hardhat project

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,43 @@
+import 'dotenv/config'
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox-viem'
+
+const DEPLOYER_KEY = process.env.DEPLOYER_KEY
+const OP_ETHERSCAN_API_KEY = process.env.OP_ETHERSCAN_API_KEY
+
+if (!DEPLOYER_KEY) throw new Error('DEPLOYER_KEY must be set')
+if (!OP_ETHERSCAN_API_KEY) throw new Error('OP_ETHERSCAN_API_KEY must be set')
+
+const config: HardhatUserConfig = {
+  networks: {
+    optimism_testnet: {
+      url: 'https://rpc.ankr.com/optimism_testnet',
+      accounts: [DEPLOYER_KEY],
+    },
+    optimism: {
+      url: 'https://rpc.ankr.com/optimism',
+      accounts: [DEPLOYER_KEY],
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.21',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: {
+      optimisticGoerli: OP_ETHERSCAN_API_KEY,
+      optimisticEthereum: OP_ETHERSCAN_API_KEY,
+    },
+  },
+}
+
+export default config
---
+++ contracts/scripts/deploy.ts
@@ -0,0 +1,32 @@
+import { formatEther, parseEther } from 'viem'
+import hre from 'hardhat'
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000)
+  const unlockTime = BigInt(currentTimestampInSeconds + 60)
+  const args = [unlockTime]
+
+  const lockedAmount = parseEther('0.001')
+
+  const lock = await hre.viem.deployContract('Lock', args, {
+    value: lockedAmount,
+  })
+
+  console.log(
+    `Lock with ${formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  )
+
+  await hre.run('verify:verify', {
+    address: lock.address,
+    constructorArguments: args,
+  })
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
